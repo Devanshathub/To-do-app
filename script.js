c36d7136b8cda11563b0e2fe08157a82a0d5544c
Devanshathub <108760232+Devanshathub@users.noreply.github.com>
2023-08-18 23:18:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const inputBox = document.getElementById("input-box"); // Use getElementById, not getElementsById
+const listContainer = document.getElementById("list-container"); // Use getElementById, not getElementsById
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"; // Use innerHTML, not innerHtml
+        li.appendChild(span);
+    }
+    
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    if (localStorage.getItem("data")) {
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
+}
+
+showTask();
+
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    if (localStorage.getItem("data")) {
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
+}
+
+showTask();
